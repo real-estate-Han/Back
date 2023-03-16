a9f51bf80ef57b57a7e7b247f6f33bb6c6543014
momoco-git <gnspope@naver.com>
2023-03-16 12:39:37 +0900

    graphql post api

--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -28,15 +28,11 @@ export const resolvers = {
     login: async function (_root, { email, password }) {
       const user = await User.findOne({ email: email });
       if (!user) {
-        throw new Error('allUsers query failed');
-        // throw new GraphQLError('You are not authorized to perform this action.', {
-        //   extensions: {
-        //     code: 401,
-        //   },
-        // });
-        // const error = new Error('User not found.');
-        // error.code = 401;
-        // throw error;
+        throw new GraphQLError('You are not authorized to perform this action.', {
+          extensions: {
+            code: 401,
+          },
+        });
       }
       const isEqual = await bcrypt.compare(password, user.password);
       if (!isEqual) {
@@ -50,9 +46,84 @@ export const resolvers = {
           email: user.email,
         },
         process.env.JWT_SECRET,
-        { expiresIn: '3h' }
+        { expiresIn: '5h' }
       );
       return { token: `Barear ${token}`, userId: user._id.toString() };
+    },
+
+    posts: async function (_root, { page }, req) {
+      if (!req.isAuth) {
+        throw new GraphQLError('Not authenticated!');
+      }
+      if (!page) {
+        page = 1;
+      }
+      const perPage = 2;
+      const totalPosts = await Post.find().countDocuments();
+      const posts = await Post.find()
+        .sort({ createdAt: -1 })
+        .skip((page - 1) * perPage)
+        .limit(perPage)
+        .populate('creator');
+      return {
+        posts: posts.map((p) => {
+          return {
+            ...p._doc,
+            _id: p._id.toString(),
+            createdAt: p.createdAt.toISOString(),
+            updatedAt: p.updatedAt.toISOString(),
+          };
+        }),
+        totalPosts: totalPosts,
+      };
+    },
+
+    post: async function (_root, { id }, req) {
+      if (!req.isAuth) {
+        throw new GraphQLError('Not authenticated!', {
+          extensions: {
+            code: 401,
+          },
+        });
+      }
+      const post = await Post.findById(id).populate('creator');
+      if (!post) {
+        throw new GraphQLError('No post found!', {
+          extensions: {
+            code: 401,
+          },
+        });
+      }
+      return {
+        ...post._doc,
+        _id: post._id.toString(),
+        createdAt: post.createdAt.toISOString(),
+        updatedAt: post.updatedAt.toISOString(),
+      };
+    },
+    deletePost: async function (_root, { id }, req) {
+      if (!req.isAuth) {
+        const error = new Error('Not authenticated!');
+        error.code = 401;
+        throw error;
+      }
+      const post = await Post.findById(id);
+      if (!post) {
+        const error = new Error('No post found!');
+        error.code = 404;
+        throw error;
+      }
+      if (post.creator.toString() !== req.userId.toString()) {
+        const error = new Error('Not authorized!');
+        error.code = 403;
+        throw error;
+      }
+      clearImage(post.imageUrl);
+      await Post.findByIdAndRemove(id);
+      const user = await User.findById(req.userId);
+      user.posts.pull(id);
+      await user.save();
+      return true;
     },
   },
   Mutation: {
--- model/post.js
+++ model/post.js
@@ -3,18 +3,31 @@ const Schema = mongoose.Schema;
 
 const postSchema = new Schema(
   {
-    title: {
-      type: String,
-      required: true,
-    },
-    imageUrl: {
-      type: String,
-      required: true,
-    },
-    content: {
-      type: String,
-      required: true,
-    },
+    itemUniqueID: { type: number, required: true },
+    itemAddress: { type: String, required: true },
+    itemGeoLocation: [{ type: Number, required: true }],
+    itemType: { type: String, required: true },
+    itemDeposit: { type: Number },
+    itemMonthly: { type: Number },
+    itemJense: { type: Number },
+    itemSale: { type: Number },
+    itemManagement: { type: Number },
+    itemAreaLand: { type: Number },
+    itemAreaBuilding: { type: Number },
+    itemFloor: { type: Number },
+    itemPurpose: { type: String },
+    itemRooms: { type: String },
+    itemStatus: { type: String },
+    itemField: { type: String },
+    itemMovein: { type: String },
+    itemApproval: { type: String },
+    itemSubway: { type: String },
+    itemTitleimg: { type: String },
+    itemDetailimg: [{ type: String }],
+    itemTag: [{ type: String }],
+    itemElectricity: { type: Number },
+    itemOption: [{ type: String }],
+
     creator: {
       type: Schema.Types.ObjectId,
       ref: 'User',
